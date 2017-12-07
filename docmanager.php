e64d9204dc6f6499933b3c5ae55b738fa6762450
Pierre Vert <vert@obs-mip.fr>
2017-12-07 17:17:30 +0100

    first commit

---
+++ docmanager.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+* Plugin Name: Sedoo Doc Manager
+* Plugin URI : https://github.com/sedoo/sedoo-wppl-docmanager
+* Description: Plugin pour gérer des documents
+* Author: Pierre VERT
+* Version: 0.1.0
+* GitHub Plugin URI: sedoo/sedoo-wppl-docmanager
+* GitHub Branch:     master
+*/
